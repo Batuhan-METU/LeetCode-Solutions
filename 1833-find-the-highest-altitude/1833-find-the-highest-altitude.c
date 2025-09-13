c358ec758e7afe7a62eb65c43fea1e2da74f6095
batuhanbasoda <e268257@metu.edu.tr>
2025-09-13 18:18:16 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1833-find-the-highest-altitude/1833-find-the-highest-altitude.c
@@ -0,0 +1,22 @@
+int largestAltitude(int* gain, int gainSize) {
+    int *result = NULL;
+
+    result = (int*)malloc((gainSize + 1) * sizeof(int));
+
+    result[0] = 0;
+    for (int i = 1; i<gainSize; i++)
+    {
+        result[i] = result[i-1] + gain[i-1]; 
+    }
+    int max = result[0];
+
+    for (int k = 0; k<gainSize+1; k++)
+    {
+        if (max<result[k])
+        {
+            max = result[k]; 
+        }
+    }
+
+    return max; 
+}
